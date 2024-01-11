86b4abfe94e394c591204cfab0360a4a7ad32f82
Arif Ibrahim <arif.ibrahm@gmail.com>
2024-01-12 02:24:26 +0600

    make video list responsive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,10 +9,10 @@ const videoList = useVideoStore()
 <template>
   <div>
     <h1 class="text-2xl font-bold mb-4">Video List</h1>
-    <div class="grid grid-cols-4 gap-4">
+    <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-2 md:gap-4">
       <div v-for="video in videoList.videos">
         <div class="">
-          <img class="w-56 h-32 object-cover" :src="video.thumbnail" alt="">
+          <img class=" object-cover" :src="video.thumbnail" alt="">
         </div>
         <RouterLink :to="{name: 'detailview', params: {id: video.id}}"><button class="bg-gray-100 hover:bg-gray-400 hover:text-gray-100 w-full">watch</button></RouterLink>
         
