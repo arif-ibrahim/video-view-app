5af5630b9f4962a61a6b00a1c011b5287ef1a574
Arif Ibrahim <arif.ibrahm@gmail.com>
2024-01-12 22:21:48 +0600

    format some line of code

--- src/stores/video.ts
+++ src/stores/video.ts
@@ -10,6 +10,8 @@ export const useVideoStore = defineStore('video', {
             { id: 4, title: 'Video 4', thumbnail: 'https://peach.blender.org/wp-content/uploads/title_anouncement.jpg?x11217', src: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4' },
             { id: 5, title: 'Video 5', thumbnail: 'https://peach.blender.org/wp-content/uploads/title_anouncement.jpg?x11217', src: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4' },
             { id: 6, title: 'Video 6', thumbnail: 'https://peach.blender.org/wp-content/uploads/title_anouncement.jpg?x11217', src: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4' },
+            { id: 2, title: 'Video 2', thumbnail: 'https://upload.wikimedia.org/wikipedia/commons/e/e8/Elephants_Dream_s5_both.jpg', src: 'https://archive.org/download/ElephantsDream/ed_hd.ogv' },
+
 
         ],
     }),
